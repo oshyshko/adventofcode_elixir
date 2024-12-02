aeddac173d078c85149c8b3d666875a228a8f6f9
Alex Shyshko <oshyshko@gmail.com>
2024-12-02 13:43:14 -0500

    Cleanup

--- lib/Y24/D01.ex
+++ lib/Y24/D01.ex
@@ -1,32 +1,30 @@
 defmodule Y24.D01 do
   import Adventofcode
 
-  # string -> [xs, ys]
-  def parse(s),
-    do:
-      lines(s)
-      |> map(&words/1)
-      |> mmap(&String.to_integer/1)
-      |> map(&List.to_tuple/1)
-      |> Enum.unzip()
-      |> Tuple.to_list()
+  # string -> {xs, ys}
+  def parse(s) do
+    lines(s)
+    |> map(&words/1)
+    |> mmap(&String.to_integer/1)
+    |> map(&List.to_tuple/1)
+    |> Enum.unzip()
+  end
 
   # string -> int
-  def solve1(s),
-    do:
-      parse(s)
-      |> map(&Enum.sort/1)
-      |> Enum.zip_with(fn [x, y] -> abs(x - y) end)
-      |> Enum.sum()
+  def solve1(s) do
+    parse(s)
+    |> Tuple.to_list()
+    |> map(&Enum.sort/1)
+    |> Enum.zip_with(fn [x, y] -> abs(x - y) end)
+    |> Enum.sum()
+  end
 
   # string -> int
-  def solve2(s),
-    do:
-      parse(s)
-      |> app(fn xs, ys ->
-        x2c = Enum.frequencies(ys)
+  def solve2(s) do
+    {xs, ys} = parse(s)
+    x2c = Enum.frequencies(ys)
 
-        map(xs, &(&1 * Map.get(x2c, &1, 0)))
-        |> Enum.sum()
-      end)
+    map(xs, &(&1 * Map.get(x2c, &1, 0)))
+    |> Enum.sum()
+  end
 end
