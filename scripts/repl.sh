ba06334990ee98cf9174b774e659ee99f8639d15
Alex Shyshko <oshyshko@gmail.com>
2024-12-04 17:16:47 -0500

    Add repl history

--- scripts/repl.sh
+++ scripts/repl.sh
@@ -5,4 +5,7 @@ set -xue
 # jump to project directory
 cd "$(dirname $0)/../"
 
+# from https://stackoverflow.com/a/45405071/107341
+export ERL_AFLAGS="-kernel shell_history enabled -kernel shell_history_path \'.iex.history\'"
+
 iex -S mix
