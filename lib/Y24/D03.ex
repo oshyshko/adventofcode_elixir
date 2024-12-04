44e3d51207aadd66a20cc78bad47512dbaed9a0e
Alex Shyshko <oshyshko@gmail.com>
2024-12-04 17:16:10 -0500

    Add Y24.D03

---
+++ lib/Y24/D03.ex
@@ -0,0 +1,34 @@
+defmodule Y24.D03 do
+  import Adventofcode
+
+  # string -> [:do | :dont | {:mul, x, y}]
+  def parse(s) do
+    Regex.scan(~r/(mul)\((\d+),(\d+)\)|do\(\)|don't\(\)/u, s)
+    |> map(fn
+      [_, "mul", x, y] -> {:mul, String.to_integer(x), String.to_integer(y)}
+      ["do()"] -> :do
+      ["don't()"] -> :dont
+    end)
+  end
+
+  def eval({:mul, x, y}), do: x * y
+  def eval(_), do: 0
+
+  # string -> int
+  def solve1(s), do: parse(s) |> map(&eval/1) |> sum
+
+  def solve2(s) do
+    do_dont = fn
+      :do, {xs, _} -> {xs, :do}
+      :dont, {xs, _} -> {xs, :dont}
+      {:mul, x, y}, {xs, :do} -> {xs ++ [{:mul, x, y}], :do}
+      {:mul, _, _}, s -> s
+    end
+
+    parse(s)
+    |> Enum.reduce({[], :do}, do_dont)
+    |> elem(0)
+    |> map(&eval/1)
+    |> sum
+  end
+end
