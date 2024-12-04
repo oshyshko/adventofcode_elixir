17e87708114f998daf0017ecd277648d4daddf01
Alex Shyshko <oshyshko@gmail.com>
2024-12-04 00:53:36 -0500

    Cleanup

--- lib/Y24/D01.ex
+++ lib/Y24/D01.ex
@@ -1,30 +1,30 @@
 defmodule Y24.D01 do
   import Adventofcode
 
-  # string -> {xs, ys}
+  # string -> {[int], [int]}
   def parse(s) do
     lines(s)
     |> map(&words/1)
     |> mmap(&String.to_integer/1)
     |> map(&List.to_tuple/1)
-    |> Enum.unzip()
+    |> unzip()
   end
 
   # string -> int
   def solve1(s) do
     parse(s)
     |> Tuple.to_list()
-    |> map(&Enum.sort/1)
-    |> Enum.zip_with(fn [x, y] -> abs(x - y) end)
-    |> Enum.sum()
+    |> map(&sort/1)
+    |> zip_with(fn [x, y] -> abs(x - y) end)
+    |> sum()
   end
 
   # string -> int
   def solve2(s) do
     {xs, ys} = parse(s)
-    x2c = Enum.frequencies(ys)
+    x2c = frequencies(ys)
 
     map(xs, &(&1 * Map.get(x2c, &1, 0)))
-    |> Enum.sum()
+    |> sum()
   end
 end
