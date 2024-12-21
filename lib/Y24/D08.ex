ba943cc9feabc95566a3cc4f7a266be830b6adc2
Alex Shyshko <oshyshko@gmail.com>
2024-12-21 02:13:40 -0500

    Cleanup

--- lib/Y24/D08.ex
+++ lib/Y24/D08.ex
@@ -5,14 +5,14 @@ defmodule Y24.D08 do
 
   def parse(s), do: lines(s) |> Vec.from_list(2)
 
-  def solve(s, f) do
+  def solve(s, f_vabd) do
     v = parse(s)
 
     Vec.each(v)
     |> group_by(&Vec.at(v, &1))
     |> Map.delete(hd(~c"."))
     |> Map.values()
-    |> map(fn ps -> for a <- ps, b <- ps, a != b, d = Vec.p_sub(a, b), do: f.(v, a, b, d) end)
+    |> map(fn ps -> for a <- ps, b <- ps, a != b, d = Vec.p_sub(a, b), do: f_vabd.(v, a, b, d) end)
     |> List.flatten()
     |> filter(&Vec.member?(v, &1))
     |> uniq()
@@ -22,8 +22,8 @@ defmodule Y24.D08 do
   def solve1(s), do: solve(s, fn _, a, b, d -> [Vec.p_add(a, d), Vec.p_sub(b, d)] end)
 
   def solve2(s) do
-    beam = fn v, p, d, f ->
-      take_while(Stream.iterate(p, &f.(&1, d)), &Vec.member?(v, &1))
+    beam = fn v, p, d, f_pd ->
+      take_while(Stream.iterate(p, &f_pd.(&1, d)), &Vec.member?(v, &1))
     end
 
     solve(s, fn v, a, b, d ->
